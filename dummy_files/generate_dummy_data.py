d250257f2be8a7056cd8243efbb5156f28cbc323
Zaki <muhammadfarid.zaki@gmail.com>
2023-04-16 23:10:01 +0700

    updates

--- dummy_files/generate_dummy_data.py
+++ dummy_files/generate_dummy_data.py
@@ -30,33 +30,33 @@ def create_users(city_table):
         user_id=i+1
         name = fake.name()
         phone_number = fake.phone_number()
-        domicile_city = city_table[random.randint(0, 14)]['city_id']
-        users.append([user_id,name, phone_number, domicile_city])
+        city_id = city_table[random.randint(0, 14)]['city_id']
+        users.append([user_id,name, phone_number, city_id])
 
-    headers = ["user_id","name", "phone_number", "domicile"]
+    headers = ["user_id","name", "phone_number", "city_id"]
     # Use tabulate to create a table
     # table = tabulate(users, headers=headers)
 
     # Print the table to the console
     # print(table)
-    create_csv("user",users,headers)
+    create_csv("users",users,headers)
 
 def create_advertisement(car_table):
     # Create fake data using the Faker library
     fake = Faker('id_ID')  # Use Indonesian locale
     advertisement = []
-    for i in range(100):
+    for i in range(30):
         text= fake.text()
         advertisement_id=i+1
-        car_product_id = car_table[random.randint(1, 49)]['product_id']
+        product_id = car_table[random.randint(1, 49)]['product_id']
         user_id = random.randint(1, 100)
         is_allowed_bid=random.choice([True, False])
         title = text.rsplit()[0]
         description= text
         timestamp_ad=fake.date_time_between(start_date='-2y', end_date='now')
-        advertisement.append([advertisement_id,car_product_id,user_id,is_allowed_bid,title,description,timestamp_ad])
+        advertisement.append([advertisement_id,product_id,user_id,is_allowed_bid,title,description,timestamp_ad])
 
-    headers = ['advertisement_id','car_product_id','user_id','is_allowed_bid','title','description','timestamp_ad']
+    headers = ['advertisement_id','product_id','user_id','is_allowed_bid','title','description','timestamp_ad']
     # Use tabulate to create a table
     # table = tabulate(advertisement, headers=headers)
 
@@ -71,12 +71,13 @@ def create_bid():
     for i in range(100):
         bid_id=i+1
         user_id = random.randint(1, 100)
-        advertisement_id=random.randint(1, 100)
+        advertisement_id=random.randint(1, 29)
         timestamp_bid=fake.date_time_between(start_date='-2y', end_date='now')
         status_bid= random.choice(["Succeed","Rejected","Pending", "Canceled"])
-        bids.append([bid_id,user_id,advertisement_id,status_bid,timestamp_bid])
+        price_bid=random.randint(60000000, 415000000)
+        bids.append([bid_id,user_id,advertisement_id,price_bid,status_bid,timestamp_bid])
 
-    headers = ['bid_id','user_id','advertisement_id','status_bid','timestamp_bid']
+    headers = ['bid_id','user_id','advertisement_id','price_bid','status_bid','timestamp_bid']
     # Use tabulate to create a table
     # table = tabulate(bids, headers=headers)
 
@@ -84,6 +85,6 @@ def create_bid():
     # print(table)
     create_csv("bid",bids,headers)
 
-create_users(csv_to_dict('dummy_files/city.csv'))
-create_advertisement(csv_to_dict('dummy_files/car_product.csv'))
+# create_users(csv_to_dict('dummy_files/city.csv'))
+# create_advertisement(csv_to_dict('dummy_files/car_product.csv'))
 create_bid()
