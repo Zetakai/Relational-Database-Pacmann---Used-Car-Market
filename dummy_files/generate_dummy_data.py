0cfee8daa4a4c4eeee1bf0aff19c68cd7d469603
Zaki <muhammadfarid.zaki@gmail.com>
2023-04-16 17:50:53 +0700

    added dummy files and script to generate them in python

---
+++ dummy_files/generate_dummy_data.py
@@ -0,0 +1,89 @@
+import csv
+from faker import Faker
+from tabulate import tabulate
+import random
+import os
+import datetime
+
+def create_csv(table_name, data, headers):
+    # Export the table to a CSV file
+    file_path = os.path.join(os.getcwd(), "dummy_files/"+table_name+".csv")
+    with open(file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(headers)
+        writer.writerows(data)
+
+def csv_to_dict(file_name):
+    file_path = os.path.join(os.getcwd(), file_name)
+    with open(file_path, 'r', encoding='utf-8-sig') as file:
+        reader = csv.DictReader(file)
+        data = []
+        for row in reader:
+            data.append(row)
+        return data
+
+def create_users(city_table):
+    # Create fake data using the Faker library
+    fake = Faker('id_ID')  # Use Indonesian locale
+    users = []
+    for i in range(100):
+        user_id=i+1
+        name = fake.name()
+        phone_number = fake.phone_number()
+        domicile_city = city_table[random.randint(0, 14)]['city_id']
+        users.append([user_id,name, phone_number, domicile_city])
+
+    headers = ["user_id","name", "phone_number", "domicile"]
+    # Use tabulate to create a table
+    # table = tabulate(users, headers=headers)
+
+    # Print the table to the console
+    # print(table)
+    create_csv("user",users,headers)
+
+def create_advertisement(car_table):
+    # Create fake data using the Faker library
+    fake = Faker('id_ID')  # Use Indonesian locale
+    advertisement = []
+    for i in range(100):
+        text= fake.text()
+        advertisement_id=i+1
+        car_product_id = car_table[random.randint(1, 49)]['product_id']
+        user_id = random.randint(1, 100)
+        is_allowed_bid=random.choice([True, False])
+        title = text.rsplit()[0]
+        description= text
+        timestamp_ad=fake.date_time_between(start_date='-2y', end_date='now')
+        advertisement.append([advertisement_id,car_product_id,user_id,is_allowed_bid,title,description,timestamp_ad])
+
+    headers = ['advertisement_id','car_product_id','user_id','is_allowed_bid','title','description','timestamp_ad']
+    # Use tabulate to create a table
+    # table = tabulate(advertisement, headers=headers)
+
+    # Print the table to the console
+    # print(table)
+    create_csv("advertisement",advertisement,headers)
+
+def create_bid():
+    # Create fake data using the Faker library
+    fake = Faker('id_ID')  # Use Indonesian locale
+    bids = []
+    for i in range(100):
+        bid_id=i+1
+        user_id = random.randint(1, 100)
+        advertisement_id=random.randint(1, 100)
+        timestamp_bid=fake.date_time_between(start_date='-2y', end_date='now')
+        status_bid= random.choice(["Succeed","Rejected","Pending", "Canceled"])
+        bids.append([bid_id,user_id,advertisement_id,status_bid,timestamp_bid])
+
+    headers = ['bid_id','user_id','advertisement_id','status_bid','timestamp_bid']
+    # Use tabulate to create a table
+    # table = tabulate(bids, headers=headers)
+
+    # Print the table to the console
+    # print(table)
+    create_csv("bid",bids,headers)
+
+create_users(csv_to_dict('dummy_files/city.csv'))
+create_advertisement(csv_to_dict('dummy_files/car_product.csv'))
+create_bid()
